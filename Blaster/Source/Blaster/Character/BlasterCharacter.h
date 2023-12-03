e853a5bafd77a807e8f2bad9f75253f2157d1f85
四月薰香满樱落 <70734031+syxxmyl@users.noreply.github.com>
2023-12-03 19:41:34 +0800

    3.15. Network Role
    -create a widget to show player net role mode above character.

--- Blaster/Source/Blaster/Character/BlasterCharacter.h
+++ Blaster/Source/Blaster/Character/BlasterCharacter.h
@@ -9,6 +9,7 @@
 
 class USpringArmComponent;
 class UCameraComponent;
+class UWidgetComponent;
 
 UCLASS()
 class BLASTER_API ABlasterCharacter : public ACharacter
@@ -35,4 +36,6 @@ private:
 	UPROPERTY(VisibleAnywhere, Category = Camera)
 	UCameraComponent* FollowCamera;
 
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
+	UWidgetComponent* OverheadWidget;
 };
