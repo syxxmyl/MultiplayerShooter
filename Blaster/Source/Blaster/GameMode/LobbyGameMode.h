e6cf93e65e0f1e0ed17ef73ba65737a45dced9b3
四月薰香满樱落 <70734031+syxxmyl@users.noreply.github.com>
2023-12-03 18:27:30 +0800

    3.14. Seamless Travel and Lobby
    -add a lobby game mode, when player number reach minimum number of start game, we'll call server travel to make all players travel to game map.
    -by adding a transition level, we can use seamless travel to improve performance.

---
+++ Blaster/Source/Blaster/GameMode/LobbyGameMode.h
@@ -0,0 +1,23 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameMode.h"
+#include "LobbyGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BLASTER_API ALobbyGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+public:
+	virtual void PostLogin(APlayerController* NewPlayer) override;
+
+private:
+	UPROPERTY(EditDefaultsOnly, Category = Rules)
+	int32 MinimumNumberToStartGame{ 2 };
+};
