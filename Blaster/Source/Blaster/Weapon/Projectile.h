96ffc8d63520d52889cc6058ade9dc2dc41db157
四月薰香满樱落 <70734031+syxxmyl@users.noreply.github.com>
2023-12-07 17:01:05 +0800

    5.1. Projectile Weapon Class
    -create a projectileweapon class and a projectile class.

---
+++ Blaster/Source/Blaster/Weapon/Projectile.h
@@ -0,0 +1,28 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Projectile.generated.h"
+
+
+class UBoxComponent;
+
+UCLASS()
+class BLASTER_API AProjectile : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	AProjectile();
+	virtual void Tick(float DeltaTime) override;
+
+protected:
+	virtual void BeginPlay() override;
+	UPROPERTY(EditAnywhere)
+	UBoxComponent* CollisionBox;
+
+private:
+
+};
