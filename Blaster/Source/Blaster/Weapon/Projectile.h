35feae9e9b93d993c8d5e03a3b6e1d9b58f85758
四月薰香满樱落 <70734031+syxxmyl@users.noreply.github.com>
2023-12-08 21:02:35 +0800

    5.10. Projectile Hit Events
    -when projectile destoryed,play sound and spawn emitter.

--- Blaster/Source/Blaster/Weapon/Projectile.h
+++ Blaster/Source/Blaster/Weapon/Projectile.h
@@ -11,6 +11,7 @@ class UBoxComponent;
 class UProjectileMovementComponent;
 class UParticleSystem;
 class UParticleSystemComponent;
+class USoundCue;
 
 UCLASS()
 class BLASTER_API AProjectile : public AActor
@@ -20,9 +21,13 @@ class BLASTER_API AProjectile : public AActor
 public:	
 	AProjectile();
 	virtual void Tick(float DeltaTime) override;
+	virtual void Destroyed() override;
 
 protected:
 	virtual void BeginPlay() override;
+
+	UFUNCTION()
+	virtual void OnHit(UPrimitiveComponent* HitComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit);
 
 private:
 	UPROPERTY(EditAnywhere)
@@ -35,4 +40,10 @@ private:
 	UParticleSystem* Tracer;
 
 	UParticleSystemComponent* TracerComponent;
+
+	UPROPERTY(EditAnywhere)
+	UParticleSystem* ImpactParticles;
+
+	UPROPERTY(EditAnywhere)
+	USoundCue* ImpactSound;
 };
