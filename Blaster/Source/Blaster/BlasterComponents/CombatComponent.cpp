740eb1cd3b12692dc9244dd3fac6ef1677c6d57a
四月薰香满樱落 <70734031+syxxmyl@users.noreply.github.com>
2023-12-22 19:51:46 +0800

    Only reload when equipped weapon is not full ammo.

--- Blaster/Source/Blaster/BlasterComponents/CombatComponent.cpp
+++ Blaster/Source/Blaster/BlasterComponents/CombatComponent.cpp
@@ -416,7 +416,7 @@ void UCombatComponent::InitializeCarriedAmmo()
 
 void UCombatComponent::Reload()
 {
-	if (CarriedAmmo > 0 && CombatState == ECombatState::ECS_Unoccupied)
+	if (CarriedAmmo > 0 && CombatState == ECombatState::ECS_Unoccupied && EquippedWeapon && !EquippedWeapon->IsFull())
 	{
 		ServerReload();
 	}
