fd732d42d0094b8815794e0791ab83ee5ba9e569
四月薰香满樱落 <70734031+syxxmyl@users.noreply.github.com>
2024-01-05 20:04:14 +0800

    14.2. Teams Game Mode
    -add a new TeamsGameMode derived from BlasterGameMode to deal with teams.

---
+++ Blaster/Source/Blaster/GameMode/TeamsGameMode.cpp
@@ -0,0 +1,83 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "TeamsGameMode.h"
+#include "Blaster/PlayerState/BlasterPlayerState.h"
+#include "Blaster/GameState/BlasterGameState.h"
+#include "Kismet/GameplayStatics.h"
+#include "Blaster/BlasterTypes/Team.h"
+
+
+void ATeamsGameMode::PostLogin(APlayerController* NewPlayer)
+{
+	Super::PostLogin(NewPlayer);
+
+	ABlasterGameState* BGameState = Cast<ABlasterGameState>(UGameplayStatics::GetGameState(this));
+	if (BGameState && NewPlayer)
+	{
+		ABlasterPlayerState* BPState = NewPlayer->GetPlayerState<ABlasterPlayerState>();
+		if (BPState && BPState->GetTeam() == ETeam::ET_NoTeam)
+		{
+			if (BGameState->BlueTeam.Num() >= BGameState->RedTeam.Num())
+			{
+				BGameState->RedTeam.AddUnique(BPState);
+				BPState->SetTeam(ETeam::ET_RedTeam);
+			}
+			else
+			{
+				BGameState->BlueTeam.AddUnique(BPState);
+				BPState->SetTeam(ETeam::ET_BlueTeam);
+			}
+		}
+
+	}
+}
+
+void ATeamsGameMode::HandleMatchHasStarted()
+{
+	Super::HandleMatchHasStarted();
+
+	ABlasterGameState* BGameState = Cast<ABlasterGameState>(UGameplayStatics::GetGameState(this));
+	if (BGameState)
+	{
+		for (auto PState : BGameState->PlayerArray)
+		{
+			ABlasterPlayerState* BPState = Cast<ABlasterPlayerState>(PState.Get());
+			if (BPState && BPState->GetTeam() == ETeam::ET_NoTeam)
+			{
+				if (BGameState->BlueTeam.Num() >= BGameState->RedTeam.Num())
+				{
+					BGameState->RedTeam.AddUnique(BPState);
+					BPState->SetTeam(ETeam::ET_RedTeam);
+				}
+				else
+				{
+					BGameState->BlueTeam.AddUnique(BPState);
+					BPState->SetTeam(ETeam::ET_BlueTeam);
+				}
+			}
+		}
+	}
+}
+
+void ATeamsGameMode::Logout(AController* Exiting)
+{
+	if (Exiting)
+	{
+		ABlasterGameState* BGameState = Cast<ABlasterGameState>(UGameplayStatics::GetGameState(this));
+		ABlasterPlayerState* BPState = Exiting->GetPlayerState<ABlasterPlayerState>();
+		if (BGameState && BPState)
+		{
+			if (BGameState->RedTeam.Contains(BPState))
+			{
+				BGameState->RedTeam.Remove(BPState);
+			}
+			if (BGameState->BlueTeam.Contains(BPState))
+			{
+				BGameState->BlueTeam.Remove(BPState);
+			}
+		}
+	}
+
+	Super::Logout(Exiting);
+}
